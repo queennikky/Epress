c4c2aca535ea419ef972c6a9419e6dc69757a3c6
queennikky <nikkycraft01@gmail.com>
2023-07-26 15:34:38 +0100

    first commit

---
+++ data/meals.js
@@ -0,0 +1,27 @@
+const meals = [
+  {
+    id: 1,
+    name: "Rice Noodles",
+  },
+  {
+    id: 2,
+    name: "Jollof Rice",
+  },
+  {
+    id: 3,
+    name: "Fried Rice",
+  },
+  {
+    id: 4,
+    name: "Ofada Rice",
+  },
+  {
+    id: 5,
+    name: "Loaded Hot Dog",
+  },
+  {
+    id: 6,
+    name: "Basmati Rice",
+  },
+];
+module.exports = meals;
